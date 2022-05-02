70f566d84d22ec65c3cf2d0cc759fc8349f94a33
kalille Overton <>
2022-05-02 17:02:22 -0400

    commit tree 63d272ffdb6d4830de9dfb32197b0aa6cd374d8f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,31 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  #   before_action :authorize
+
+
+  #   private
+  #   def current_user
+  #     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+
+  # def authorize
+  #   return render json: {error:'Not Authorized'}, status: :unauthorized unless @current_user
+  # end
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  before_action :authorize
+
+  private
 
   def authorize
-    return render json: {error:'Not Authorized'}, status: :unauthorized unless session.include? :user_id
+    @current_user = User.find_by(id: session[:user_id])
+
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 
